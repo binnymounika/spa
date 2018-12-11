ae14333965a11da0e0d439ae03cd598c14fef693
R N <720432@cognizant.com>
2018-12-11 16:00:06 +0530

    spa

---
+++ src/spa/content/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {CSSTransition} from 'react-transition-group'
+class Home extends React.Component {
+    render() { 
+        return ( 
+            <CSSTransition
+            in={true}
+            appear={true}
+            timeout={1000}
+            classNames={"fade"}
+            enter={false}>
+            <div>Am at Home</div>
+            </CSSTransition>
+            
+         );
+    }
+}
+ 
+export default Home;
